08ae1c305e7a10f3a775e1da85a2c5da342a1263
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-11-01 15:27:29 +0600

    auth ui

--- app/(site)/Components/AuthForm.tsx
+++ app/(site)/Components/AuthForm.tsx
@@ -4,66 +4,125 @@ import Button from "@/app/Components/Button";
 import Input from "@/app/Components/Input/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>("LOGIN");
-    const [isLoading, setIsLoading] = useState(false);
-    
-    const toggleVariant = useCallback(() => {
-        if (variant === "LOGIN") {
-            setVariant('REGISTER');
-        } else {
-            setVariant('LOGIN');
-        }
-    }, [variant])
-    
-    const { register, handleSubmit, formState: {
-        errors
-    } } = useForm<FieldValues>({
-        defaultValues: {
-            name: '',
-            email: '',
-            password: '',
-        },
+  const [isLoading, setIsLoading] = useState(false);
 
-    })
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
 
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true);
-        
-        if (variant === 'REGISTER') {
-            // axios register
-        }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-        if (variant === 'LOGIN') {
-            //axios login
-        }
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      // axios register
     }
 
-    const socialAction = (action: string) => {
-        setIsLoading(true);
-
-        //netAuth social sign in
+    if (variant === "LOGIN") {
+      //axios login
     }
+  };
 
-    return <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-                {variant === 'REGISTER' && (
-                    <Input label="Name" register={register} id="name" type="text" errors={errors} />
-                    
-                )}
-              
-                <Input label="Email" register={register} id="email" type="email" errors={errors} />
-               
-                <Input label="Password" register={register} id="password" type="password" errors={errors} />
-                <Button>Login</Button>
-            </form>
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    //netAuth social sign in
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              label="Name"
+              register={register}
+              id="name"
+              type="text"
+                          errors={errors}
+                          disabled={isLoading}
+            />
+          )}
+
+          <Input
+            label="Email"
+            register={register}
+            id="email"
+            type="email"
+                      errors={errors}
+                      disabled={isLoading}
+          />
+
+          <Input
+            label="Password"
+            register={register}
+            id="password"
+            type="password"
+                      errors={errors}
+                      disabled={isLoading}
+          />
+          <Button disabled={isLoading} type="submit" fullWidth>
+            {variant === "LOGIN" ? "Sign In" : "Register"}
+          </Button>
+        </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-orange-300" />
+            </div>
+            <div className="relative flex justify-center text-sm ">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex gap-2">
+          <AuthSocialButton
+            icon={BsGithub}
+            onClick={() => socialAction("GitHub")}
+          />
+          <AuthSocialButton
+            icon={BsGoogle}
+            onClick={() => socialAction("Google")}
+          />
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === "LOGIN"
+              ? "New to Gossipy?"
+              : "Already have an Account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an Account" : "Login"}
+          </div>
+        </div>
       </div>
-        
-  </div>;
+    </div>
+  );
 };
 
 export default AuthForm;
