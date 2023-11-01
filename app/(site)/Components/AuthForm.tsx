f055c3891264a6f7ba28d4fb755d3ace6cf85b75
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-11-01 15:05:27 +0600

    input and button

--- app/(site)/Components/AuthForm.tsx
+++ app/(site)/Components/AuthForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Button from "@/app/Components/Button";
+import Input from "@/app/Components/Input/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
@@ -46,8 +48,20 @@ const AuthForm = () => {
         //netAuth social sign in
     }
 
-    return <div>
-      
+    return <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                {variant === 'REGISTER' && (
+                    <Input label="Name" register={register} id="name" type="text" errors={errors} />
+                    
+                )}
+              
+                <Input label="Email" register={register} id="email" type="email" errors={errors} />
+               
+                <Input label="Password" register={register} id="password" type="password" errors={errors} />
+                <Button>Login</Button>
+            </form>
+      </div>
         
   </div>;
 };
