42244d13168b00bffd06bf9dfbbe0c3a8c7fa53f
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-01-07 13:05:52 +0600

    get user session and hooks

--- app/Components/Sidebar/DesktopSidebar.tsx
+++ app/Components/Sidebar/DesktopSidebar.tsx
@@ -2,11 +2,18 @@
 import useRoutes from '@/app/hooks/useRoutes';
 import React, { useState } from 'react';
 import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps{
+    currentUser: User;
+}
+
+const DesktopSidebar:React.FC<DesktopSidebarProps> = ({currentUser}) => {
     const routes = useRoutes();
     console.log(routes)
     const [isOpen, setIsOpen] = useState(false);
+
+
 
     return (
         <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4  lg:flex lg:flex-col justify-between">
