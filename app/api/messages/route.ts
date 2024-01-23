8729172730d79f82cd78b48de4b4891770176948
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-01-24 03:05:51 +0600

    pusher setup

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,7 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from '@/app/libs/prismadb'
+import { pusherServer } from "@/app/libs/pusher";
 
 export async function POST(request: Request) {
     try {
@@ -46,7 +47,7 @@ export async function POST(request: Request) {
                 lastMessageAt: new Date(),
                 messages: {
                     connect: {
-                        id:newMessage.id
+                        id: newMessage.id
                     }
                 }
             },
@@ -54,11 +55,23 @@ export async function POST(request: Request) {
                 users: true,
                 messages: {
                     include: {
-                        seen:true
+                        seen: true
                     }
                 }
             }
+        });
+
+        await pusherServer.trigger(conversationId, 'messages:new', newMessage);
+        const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1];
+
+        updatedConversation.users.map((user) => {
+            pusherServer.trigger(user.email!, 'conversation:update', {
+                id: conversationId,
+                messages: [lastMessage]
+            })
         })
+
+
         return NextResponse.json({newMessage, updatedConversation})
 
     }
